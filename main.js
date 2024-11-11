b8e83bfc9c6c6b2ee4af228244dddf853bf44df9
ceq5032 <125389208+ceq5032@users.noreply.github.com>
2024-11-11 15:13:49 -0500

    update

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
 });
 
+//to resize with screen
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
@@ -30,7 +31,7 @@ scene.add(pointLight, ambientLight);
 
 renderer.render(scene, camera);
 
-// Define textMesh
+// Define textMesh - spinning word
 let textMesh;
 
 const loader = new FontLoader();
@@ -71,6 +72,7 @@ function addStar() {
     scene.add(star);
 }
 
+// amount of stars
 Array(400).fill().forEach(addStar);
 
 // Set background texture
@@ -81,7 +83,7 @@ scene.background = spaceTexture;
 const chelsTexture = new THREE.TextureLoader().load('images/chels.jpg');
 const chels = new THREE.Mesh(
     new THREE.BoxGeometry(3, 3, 3),
-    new THREE.MeshBasicMaterial({ map: chelsTexture })
+    new THREE.MeshBasicMaterial({ map: chelsTexture }) //covers box with photo
 );
 scene.add(chels);
 chels.position.z = 45;
@@ -95,7 +97,7 @@ const normalTexture = new THREE.TextureLoader().load('images/texture2.jpg');
 const tex = new THREE.Mesh(
     new THREE.SphereGeometry(3, 32, 32),
     new THREE.MeshStandardMaterial({ map: texTexture,
-    normalMap: normalTexture })
+    normalMap: normalTexture }) //creates actual texture on image
 );
 scene.add(tex);
 
@@ -105,16 +107,16 @@ tex.position.setX(-15);
 // Move camera on scroll
 function moveCamera() {
     const t = document.body.getBoundingClientRect().top;
-    torus.rotation.x += 0.05;
+    torus.rotation.x += 0.005;
     torus.rotation.y += 0.005;
-    torus.rotation.z += 0.05;
+    torus.rotation.z += 0.005;
 
-    chels.rotation.y += 0.05;
-    chels.rotation.z += 0.05;
+    chels.rotation.y += 0.005;
+    chels.rotation.z += 0.005;
 
-    tex.rotation.x += 0.05;
+    tex.rotation.x += 0.005;
     tex.rotation.y += 0.005;
-    tex.rotation.z += 0.05;
+    tex.rotation.z += 0.005;
 
 
     if (textMesh) {
@@ -122,7 +124,7 @@ function moveCamera() {
         textMesh.rotation.y += 0.005;
     }
 
-    camera.position.z = t * -0.01;
+    camera.position.z = t * -0.01;  //position of camera
     camera.position.x = t * -0.0002;
     camera.rotation.y = t * -0.0002;
 }
