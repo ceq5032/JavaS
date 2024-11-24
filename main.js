4f7fe3f8901443150a746afa0922308c8021ac68
ceq5032 <125389208+ceq5032@users.noreply.github.com>
2024-11-24 16:45:41 -0500

    update theme

--- main.js
+++ main.js
@@ -19,14 +19,18 @@ camera.position.setZ(30);
 
 
 // Create a torus geometry and material
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0x171c52 });
-const torus = new THREE.Mesh(geometry, material);
-scene.add(torus);
+//const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+//const material = new THREE.MeshStandardMaterial({ color: 0x6d180f });
+//const torus = new THREE.Mesh(geometry, material);
+//scene.add(torus);
+//torus.position.z = -5;
+//torus.position.y = 0;
+//torus.position.x = 0;
+
 
 // Lighting setup
 const pointLight = new THREE.PointLight(0xffffff, 2);
-pointLight.position.set(2, 2, 2);
+pointLight.position.set(1, 1, 1);
 const ambientLight = new THREE.AmbientLight(0xffffff, 2);
 scene.add(pointLight, ambientLight);
 
@@ -39,28 +43,31 @@ const loader = new FontLoader();
 loader.load('https://threejs.org/examples/fonts/optimer_regular.typeface.json', function (font) {
     console.log("Font loaded successfully");
 
-    const textGeometry = new TextGeometry('Whats Up!', {
+    const textGeometry = new TextGeometry('" Y o u  w i l l  t r y . "', {
         font: font,
         size: 2,
         height: 1,
-        curveSegments: 12,
+        curveSegments: 1,
         bevelEnabled: true,
-        bevelThickness: 0.2,
-        bevelSize: 0.2,
+        bevelThickness: 0.1,
+        bevelSize: 0.1,
         bevelOffset: 0,
-        bevelSegments: 5
+        bevelSegments: 0
     });
 
-    const textMaterial = new THREE.MeshBasicMaterial({ color: 0x821591 });
+    const textMaterial = new THREE.MeshBasicMaterial({ color: 0xa21818 });
     textMesh = new THREE.Mesh(textGeometry, textMaterial);
 
-    textMesh.position.set(-5, 0, 0);
+    textMesh.position.set(-8, 0, 5);
     scene.add(textMesh);
 }, undefined, function (error) {
     console.error("Font failed to load", error);
 });
 
 const controls = new OrbitControls(camera, renderer.domElement);
+
+
+
 
 // Function to add stars
 function addStar() {
@@ -77,52 +84,111 @@ function addStar() {
 Array(400).fill().forEach(addStar);
 
 // Set background texture
-const spaceTexture = new THREE.TextureLoader().load('images/space.jpg');
+const spaceTexture = new THREE.TextureLoader().load('images/blackSpace.jpg');
 scene.background = spaceTexture;
 
-// Avatar
-const chelsTexture = new THREE.TextureLoader().load('images/chels.jpg');
-const chels = new THREE.Mesh(
+//anakin
+const anakinTexture = new THREE.TextureLoader().load('images/anakin.jpg');
+const anakin = new THREE.Mesh(
     new THREE.BoxGeometry(3, 3, 3),
-    new THREE.MeshBasicMaterial({ map: chelsTexture }) //covers box with photo
+    new THREE.MeshBasicMaterial({ map: anakinTexture }) //covers box with photo
 );
-scene.add(chels);
-chels.position.z = 45;
-chels.position.y = 0;
-chels.position.x = 5;
+scene.add(anakin);
+anakin.position.z = -5;
+anakin.position.y = 0;
+anakin.position.x = 5;
+
+// Darth
+const darthVTexture = new THREE.TextureLoader().load('images/darthV.jpg');
+const darthV = new THREE.Mesh(
+    new THREE.BoxGeometry(3, 3, 3),
+    new THREE.MeshBasicMaterial({ map: darthVTexture }) //covers box with photo
+);
+scene.add(darthV);
+darthV.position.z = 45;
+darthV.position.y = 0;
+darthV.position.x = 5;
 
 // Sphere with texture
-const texTexture = new THREE.TextureLoader().load('images/texture.jpg');
+const dStarTexture = new THREE.TextureLoader().load('images/deathStar.jpg');
 const normalTexture = new THREE.TextureLoader().load('images/texture2.jpg');
 
-const tex = new THREE.Mesh(
-    new THREE.SphereGeometry(3, 32, 32),
-    new THREE.MeshStandardMaterial({ map: texTexture,
+const dStar = new THREE.Mesh(
+    new THREE.SphereGeometry(5, 32, 32),
+    new THREE.MeshStandardMaterial({ map: dStarTexture,
     normalMap: normalTexture }) //creates actual texture on image
 );
-scene.add(tex);
+scene.add(dStar);
 
-tex.position.z = 55;
-tex.position.setX(-15);
+dStar.position.z = 55;
+dStar.position.setX(-20);
+
+
+//tie fighter made in blender
+const loader1 = new GLTFLoader();
+
+loader1.load('images/tieFighter3.glb', (gltf) => {
+    scene.add(gltf.scene);
+    gltf.scene.rotation.x = Math.PI / 6;
+    gltf.scene.rotation.y = -Math.PI / 2;
+    gltf.scene.rotation.z = -Math.PI / 40;
+
+
+
+    gltf.scene.position.z= 44;
+    gltf.scene.position.setX(-10);
+    gltf.scene.scale.set( 2, 2, 2);
+
+}, (xhr) => {
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+}, (error) => {
+    console.error('An error occurred while loading the model.', error); 1
+});
+
+//tie fighter made in blender
+const loader2 = new GLTFLoader();
+
+loader2.load('images/tieFighter3.glb', (gltf) => {
+    scene.add(gltf.scene);
+    gltf.scene.rotation.x = Math.PI / 6;
+    gltf.scene.rotation.y = -Math.PI / 2;
+
+
+    gltf.scene.position.z= 10;
+    gltf.scene.position.setX(10);
+    gltf.scene.position.setY(10);
+    gltf.scene.scale.set( 2, 2, 2);
+
+}, (xhr) => {
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+}, (error) => {
+    console.error('An error occurred while loading the model.', error); 1
+});
+
 
 // Move camera on scroll
 function moveCamera() {
     const t = document.body.getBoundingClientRect().top;
-    torus.rotation.x += 0.005;
-    torus.rotation.y += 0.005;
-    torus.rotation.z += 0.005;
+   // torus.rotation.x += 0.005;
+    //torus.rotation.y += 0.005;
+   // torus.rotation.z += 0.005;
 
-    chels.rotation.y += 0.005;
-    chels.rotation.z += 0.005;
+    darthV.rotation.x += 0.003;
+    darthV.rotation.y += 0.003;
+    darthV.rotation.z += 0.003;
 
-    tex.rotation.x += 0.005;
-    tex.rotation.y += 0.005;
-    tex.rotation.z += 0.005;
+    dStar.rotation.x += 0.005;
+    dStar.rotation.y += 0.005;
+    dStar.rotation.z += 0.005;
+
+    anakin.rotation.x += 0.003;
+    anakin.rotation.y += 0.003;
+    anakin.rotation.z += 0.003;
 
 
     if (textMesh) {
-        textMesh.rotation.x += 0.005;
-        textMesh.rotation.y += 0.005;
+        textMesh.rotation.x += 0;
+        textMesh.rotation.y += 0;
     }
 
     camera.position.z = t * -0.01;  //position of camera
@@ -135,22 +201,26 @@ document.body.onscroll = moveCamera;
 function animate() {
     requestAnimationFrame(animate);
 
-    torus.rotation.x += 0.01;
-    torus.rotation.y += 0.005;
-    torus.rotation.z += 0.01;
+  //  torus.rotation.x += 0.01;
+  //  torus.rotation.y += 0.005;
+  //  torus.rotation.z += 0.01;
 
-    chels.rotation.y += 0.01;
-    chels.rotation.z += 0.004;
-    chels.rotation.x += 0.01;
+    darthV.rotation.y += 0.01;
+    darthV.rotation.z += 0.004;
+    darthV.rotation.x += 0.01;
 
-    tex.rotation.x += 0.01;
-    tex.rotation.y += 0.01;
-    tex.rotation.z += 0.005;
+    dStar.rotation.x += 0.01;
+    dStar.rotation.y += 0.01;
+    dStar.rotation.z += 0.005;
+
+    anakin.rotation.x += 0.005;
+    anakin.rotation.y += 0.005;
+    anakin.rotation.z += 0.005;
 
 
     if (textMesh) {
-        textMesh.rotation.x += 0.01;
-        textMesh.rotation.y += 0.01;
+        textMesh.rotation.x += 0;
+        textMesh.rotation.y += 0;
     }
 
     controls.update();
