9b25be1bf5417bab93c771b22555ba9085a102cc
ceq5032 <125389208+ceq5032@users.noreply.github.com>
2024-11-24 21:08:41 -0500

    Updates

---
+++ script.js
@@ -0,0 +1,38 @@
+const events = document.querySelectorAll('.event');
+
+events.forEach(event => {
+    event.addEventListener('mouseover', () => {
+        event.style.backgroundColor = 'rgba(139,23,23,0.55)';
+    });
+
+    event.addEventListener('mouseout', () => {
+        event.style.backgroundColor = 'transparent';
+    });
+});
+
+const timeline = document.querySelector('.timeline');
+
+timeline.addEventListener('wheel', (event) => {
+    timeline.scrollTop += event.deltaY;
+});
+
+// Timeline and transition
+const timeLine = document.querySelector('.landing');
+const transitionButton = document.getElementById('transitionButton');
+
+const goToLandingButton = document.getElementById('goToLanding');
+
+goToLandingButton.addEventListener('click', () => {
+    window.location.href = 'landing.html';
+});
+
+//  Fade Out
+goToLandingButton.addEventListener('click', () => {
+    document.body.style.transition = 'opacity 0.5s ease';
+    document.body.style.opacity = '0';
+    setTimeout(() => {
+        window.location.href = 'landing.html';
+    }, 500);
+});
+
+
